806f981566630cf323694a93b308b0b239b775e2
netovieira01_ <76048368+manoelvieira0@users.noreply.github.com>
2022-01-20 16:19:42 -0300

    O laço com while

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,16 +3,32 @@ print("Bem Vindo no Jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute = input("Digite o seu número: ")
+while(rodada <= total_de_tentativas):
 
-print("Você Digitou", chute) #Para converter Str em Int
+    print("Tentativa:", rodada, "de", total_de_tentativas)
 
-numero = int(chute)
+    chute_str = input("Digite o seu número: ")
 
-if (numero_secreto == numero):
-    print("Você acertou :)")
-else:
-    print("Você errou :(")
+    print("Você Digitou", chute_str) #Para converter Str em Int
+
+    chute = int(chute_str)
+
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if (acertou):
+        print("Você acertou :)")
+
+    else:
+        if(maior):
+            print("Você errou :( O seu número foi maior que o número secreto.")
+        elif (menor):
+            print("Você errou :( O seu número foi menor que o número secreto.")
+
+    rodada = rodada + 1
 
 print("Fim de Jogo!")
