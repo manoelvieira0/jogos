e52f8f7e6f92dc790535edc88f4a5346c938ce3d
netovieira01_ <76048368+manoelvieira0@users.noreply.github.com>
2022-01-22 22:50:28 -0300

    Encerrando a interação e o loop

--- adivinhacao.py
+++ adivinhacao.py
@@ -9,11 +9,15 @@ for rodada in range(1, total_de_tentativas + 1):
 
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
 
-    chute_str = input("Digite o seu número: ")
+    chute_str = input("Digite um número entre 1 e 100: ")
 
     print("Você Digitou", chute_str) #Para converter Str em Int
 
     chute = int(chute_str)
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -21,7 +25,7 @@ for rodada in range(1, total_de_tentativas + 1):
 
     if (acertou):
         print("Você acertou :)")
-
+        break
     else:
         if(maior):
             print("Você errou :( O seu número foi maior que o número secreto.")
