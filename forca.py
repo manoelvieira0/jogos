26af0a10ab919014b65e5c84a0464ab7f9510bd3
netovieira01_ <76048368+manoelvieira0@users.noreply.github.com>
2022-01-26 07:32:40 -0300

    Game Loop

--- forca.py
+++ forca.py
@@ -4,6 +4,14 @@ def jogar():
     print("Bem Vindo no Jogo da Forca!")
     print("*********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        print("Jogando...")
+
     print('Fim de Jogo!')
 
 if(__name__ == "__main__"):
