2b3df6bf2ec71a797f44ec7a65c30e870339c108
netovieira01_ <76048368+manoelvieira0@users.noreply.github.com>
2022-01-26 06:48:00 -0300

    Definindo uma pontuação para o usuário

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,6 +6,7 @@ print("*********************************")
 
 numero_secreto = random.randrange(1, 101)
 total_de_tentativas = 0
+pontos = 1000
 
 print("Qual o nível da dificuldade?")
 print("(1) Fácil (2) Médio (3) Difícil")
@@ -15,7 +16,7 @@ if(nivel == 1):
     total_de_tentativas = 20
 elif(nivel == 2):
     total_de_tentativas = 10
-elif(nivel == 3):
+else:
     total_de_tentativas = 5
 
 for rodada in range(1, total_de_tentativas + 1):
@@ -37,12 +38,14 @@ for rodada in range(1, total_de_tentativas + 1):
     menor = chute < numero_secreto
 
     if (acertou):
-        print("Você acertou :)")
+        print("Você acertou e fez {} pontos:)".format(pontos))
         break
     else:
         if(maior):
             print("Você errou :( O seu número foi maior que o número secreto.")
         elif (menor):
             print("Você errou :( O seu número foi menor que o número secreto.")
+        pontos_perdidos = abs(numero_secreto - chute) #40-20
+        pontos = pontos - pontos_perdidos
 
 print("Fim de Jogo!")
