7c3014403efb0c93101c20193f3ad737fd457d54
netovieira01_ <76048368+manoelvieira0@users.noreply.github.com>
2022-01-26 06:59:05 -0300

    Importando arquivos dentro de outros e criando jogo forca

---
+++ jogos.py
@@ -0,0 +1,15 @@
+import forca
+import adivinhacao
+
+print("*********************************")
+print("*******Escolha o seu Jogo!*******")
+print("*********************************")
+
+print("(1) Forca (2) Adivinhação")
+
+jogo = int(input("Qual Jogo?"))
+
+if(jogo == 1):
+    print("Jogando Forca")
+elif(jogo == 2):
+    print("Jogando Adivinhação")
