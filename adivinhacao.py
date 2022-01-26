0782490de0c07b10d2c4acfcb4e73e04cbcb7bc7
netovieira01_ <76048368+manoelvieira0@users.noreply.github.com>
2022-01-26 06:40:41 -0300

    Adicionando níveis ao jogo

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,9 +5,18 @@ print("Bem Vindo no Jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = random.randrange(1, 101)
-total_de_tentativas = 3
+total_de_tentativas = 0
 
-print(numero_secreto)
+print("Qual o nível da dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+nivel = int(input("Digite o nível: "))
+
+if(nivel == 1):
+    total_de_tentativas = 20
+elif(nivel == 2):
+    total_de_tentativas = 10
+elif(nivel == 3):
+    total_de_tentativas = 5
 
 for rodada in range(1, total_de_tentativas + 1):
 
