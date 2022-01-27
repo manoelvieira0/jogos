dbf2691b054bb0416dd45a20cfd9ae40fcfb4986
netovieira01_ <76048368+manoelvieira0@users.noreply.github.com>
2022-01-27 15:02:47 -0300

    Criando mais funções

--- forca.py
+++ forca.py
@@ -4,25 +4,21 @@ def jogar():
 
     imprime_mensagem_abertura()
     palavra_secreta = carrega_palavra_secreta()
+
     letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
+    print(letras_acertadas)
 
     enforcou = False
     acertou = False
     erros = 0
 
-    print(letras_acertadas)
 
     while(not enforcou and not acertou):
 
-        chute = input("Qual letra?")
-        chute = chute.strip().upper()
+        chute = pede_chute()
 
         if(chute in palavra_secreta):
-            index = 0
-            for letra in palavra_secreta:
-                if(chute.upper() == letra.upper()):
-                    letras_acertadas[index] = letra
-                index += 1
+            marca_chute_correto(chute, letras_acertadas, palavra_secreta)
         else:
             erros += 1
 
@@ -33,11 +29,9 @@ def jogar():
         print(letras_acertadas)
 
     if(acertou):
-        print("Você ganhou!")
+        imprime_mensagem_vencedor()
     else:
-        print("Você perdeu!")
-
-    print('Fim de Jogo!')
+        imprime_mensagem_perdedor()
 
 
 def imprime_mensagem_abertura():
@@ -62,6 +56,23 @@ def carrega_palavra_secreta():
 def inicializa_letras_acertadas(palavra):
     return ["_" for letra in palavra]
 
+def pede_chute():
+    chute = input("Qual letra?")
+    chute = chute.strip().upper()
+    return chute
+
+def marca_chute_correto(chute, letras_acertadas, palavra_secreta):
+    index = 0
+    for letra in palavra_secreta:
+        if (chute.upper() == letra.upper()):
+            letras_acertadas[index] = letra
+        index += 1
+
+def imprime_mensagem_vencedor():
+    print('Você ganhou!')
+
+def imprime_mensagem_perdedor():
+    print("Você perdeu!")
 
 if(__name__ == "__main__"):
     jogar()
